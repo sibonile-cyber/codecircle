891f11168d2986fda35feb2cefa33c053a60d10f
Sibonile Khumalo <sibonileanelekhumalo@gmail.com>
2026-04-13 22:14:17 +0700

    Created a Message page

---
+++ Pages/Messages.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CodeCircle.Pages
+{
+    public class MessagesModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
